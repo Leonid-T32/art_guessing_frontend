7bb39eeb0875ca2095a1974397d15841bab1bff2
Laila Dib <lailadib@gmail.com>
2023-06-22 12:30:14 -0300

    Last last changes

--- pages/1_about_project.py
+++ pages/1_about_project.py
@@ -12,19 +12,18 @@ st.markdown(' ')
 # Textblock1
 st.markdown('''
                 ## I think I know this art style...  or do I?
-                Imagine, **you are with other people on a party or receptio**, where several **paintings of different artists are exhibited**. 
-                You sure know the feeling "Wait, I know that art style. Or do I? Hmmm... I am not really sure".
-                Others seem to be very sure about the art style although some of their guesses appear quite bold. 
-                Some even fight over who is right or wrong, families are devided, couples split - this is when art is hard.
-                
-                Well, **our application **ARTEND** comes in handy now**.
-                You can load pictures into it by either uploading them from the web or by photographing them with your mobile phone and **ARTEND predicts the style for you**.
-                Isn't that marvelous? Now **you can be more confident on your hunch**. Of course you can guess yourself the style before **ARTEND** predicts it so you can see if you were right. 
-                Or, why don't you **use it as a party game** and be the good samaritan who keeps families and couples together? 
+                Imagine, **you are mingling with others at a party or reception**, where several **paintings by different artists are exhibited**.
+                You have the familiar feeling of "Wait, I know that art style. Or do I? Hmmm... I am not really sure".
+                Others seem overly confident about the art style, but some of their guesses appear quite bold.
+                Some even fight over who's right and who's wrong, families are divided, couples split - this is when art is hard.
+
+                Well, **our application **ARTEND** comes in handy now!**.
+                You can input artworks into it, either by uploading them from the web or by photographing them with your mobile phone, and **ARTEND recognises the style for you**.
+                Isn't that marvellous? Now **you can be more confident on your hunch**. Of course, you can submit your own guess before **ARTEND** predicts the style so you can see if you were right.
+                Or why not **use it as a party game**, become the good Samaritan who keeps families and couples together?
                 **Socialising with art, but doing it smart**. What **a jolly time**!
 
-
-                We have already many ideas how to improve and modify **ARTEND** and its neural network model that does all the work in the background in the future, **please see for perspectives the "about us" page**. 
+                While we love the app we've been able to build in two weeks, we already have a list of ideas on how to improve and further **ARTEND**. For more on the future of our app and its neural network model, **please see the "about us" page**.
                 ''')
 
 # Textblock1 - Picture1:
--- pages/3_behind_the_scenes.py
+++ pages/3_behind_the_scenes.py
@@ -12,16 +12,16 @@ st.markdown(' ')
 # Textblock2
 st.markdown('''
                 ## Approach & model - next CNN topmodel?
-                To solve the challenging task of predicting art styles, we used the **deep learning (DL)** method in which **convolutional neural networks (CNNs)** are built to analyze large datasets.
+                To solve the challenging task of predicting art styles, we used the **deep learning (DL)** method, in which **convolutional neural networks (CNNs)** are built to analyse large datasets.
                 First, we tested our **own neural models**, setting up new neural layers from scratch.
                 However, computationally and accuracy-wise, they did not perform well.
 
                 As a consequence, we switched our strategy to **transfer learning**, which means that we applied neural models that were already tested on general image identification problems.
                 To the core layers of these models, we then added our own final dense layers adapted to our dataset.
                 Overall, we tested different models: VGG-16, ResNet50, EfficientNet-B1, EfficientNet-B2, and EfficientNet-B7.
-                
+
                 In general, it seems that EfficientNet models show high performance related to image identification, as shown in this link: [https://pypi.org/project/efficientnet/](https://pypi.org/project/efficientnet/).
-                **For our dataset the best performing model was EfficientNetB2**, which we finally implemented in our application.
+                **For our dataset, the best performing model was EfficientNetB2**, which we finally implemented in our application.
                 For more details, please see the figure below.
                 ''')
 
